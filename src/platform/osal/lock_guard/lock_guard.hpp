bd9c481dcc6facec2d252c086da0321668c81122
minkyukim <mok07333@gmail.com>
2026-05-27 19:18:32 +0900

    chore(lock_guard): implement lock_guard code

---
+++ src/platform/osal/lock_guard/lock_guard.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace osal{
+
+template<typename T>
+class LockGuard{
+public:
+    explicit LockGuard(T& lock) : lock_(lock){
+        lock_.lock();
+    }
+    ~LockGuard(){
+        lock_.unlock();
+    }
+    LockGuard(const LockGuard&) = delete;
+    LockGuard& operator=(const LockGuard&) = delete;
+    LockGuard(const LockGuard&&) = delete;
+    LockGuard& operator=(const LockGuard&&) = delete;
+
+private:
+    T& lock_;
+};
+
+} // namespace osal
