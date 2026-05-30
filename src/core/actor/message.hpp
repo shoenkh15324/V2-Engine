8fd3e8cf05fce377969c0eb870fcd8f6f110860a
minkyukim <mok07333@gmail.com>
2026-05-30 19:58:22 +0900

    fix(message): Add a way to identify messages

--- src/core/actor/message.hpp
+++ src/core/actor/message.hpp
@@ -6,6 +6,13 @@ namespace core::actor{
 class Message{
 public:
     virtual ~Message() = default;
+    virtual const void* type() const noexcept = 0;
+
+    template <typename T>
+    static const void* typeId() noexcept {
+        static const char id = 0;
+        return &id;
+    }
 };
 
 using MessagePtr = std::unique_ptr<Message>;
