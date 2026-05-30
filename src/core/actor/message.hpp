e03a44af4918102f29ee56a792028cf6c23e3854
minkyukim <mok07333@gmail.com>
2026-05-30 11:34:10 +0900

    chore(message): implement message code

---
+++ src/core/actor/message.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <memory>
+
+namespace core::actor{
+
+class Message{
+public:
+    virtual ~Message() = default;
+
+};
+
+using MessagePtr = std::unique_ptr<Message>;
+
+} // namespace core::actor
