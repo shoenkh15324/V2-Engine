5f05cd741e3dad444f0558fb71f729ce6596115f
minkyukim <mok07333@gmail.com>
2026-05-30 11:58:39 +0900

    fix(mailbox): fix compile error

--- src/core/actor/mailbox.hpp
+++ src/core/actor/mailbox.hpp
@@ -6,18 +6,18 @@
 #include "../../platform/osal/lock_guard/lock_guard.hpp"
 
 namespace core::actor {
+namespace osal = platform::osal;
 
 template <typename T>
 class Mailbox {
 private:
     using Result = core::common::Result;
-    using Mutex = platform::osal::Mutex;
-    using LockGuard = platform::osal::LockGuard;
+
 public:
-    explicit Mailbox(size_t size) : buffer_(size), capacity_(size), count_(0), head_(0), tail_(0) {}
-    
+    explicit Mailbox(size_t size) : buffer_(size), capacity_(size), count_(0), head_(0), tail_(0){}
+
     Result push(const T& msg){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         if (count_ == capacity_) return Result::Fail;
         buffer_[tail_] = msg;
         tail_ = (tail_ + 1) % capacity_;
@@ -26,7 +26,7 @@ public:
     }
 
     Result push(T&& msg){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         if (count_ == capacity_) return Result::Fail;
         buffer_[tail_] = std::move(msg);
         tail_ = (tail_ + 1) % capacity_;
@@ -35,7 +35,7 @@ public:
     }
 
     Result pop(T& out){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         if (count_ == 0) return Result::Fail;
         out = std::move(buffer_[head_]);
         head_ = (head_ + 1) % capacity_;
@@ -44,27 +44,27 @@ public:
     }
 
     bool empty(){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         return count_ == 0;
     }
 
     size_t capacity(){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         return capacity_;
     }
 
     size_t count(){
-        LockGuard<Mutex> lock(mutex_);
+        osal::LockGuard<osal::Mutex> lock(mutex_);
         return count_;
     }
 
 private:
     std::vector<T> buffer_;
-    mutable Mutex mutex_;
+    mutable osal::Mutex mutex_;
     size_t capacity_;
     size_t count_;
-    size_t tail_;
     size_t head_;
+    size_t tail_;
 };
 
 } // namespace core::actor
