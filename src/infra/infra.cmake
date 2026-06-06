b7ae10a369f777a0518c3dac72ae524aed7bb0d7
minkyukim <mok07333@gmail.com>
2026-06-06 20:18:56 +0900

    feat(transport): add transport folder

--- src/infra/infra.cmake
+++ src/infra/infra.cmake
@@ -5,7 +5,8 @@ message(STATUS "Configuring Infra Layer")
 add_library(v2_infra STATIC)
 
 target_sources(v2_infra PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/hal/dummy.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/transport/uds/uds_server.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/transport/uds/uds_client.cpp
 )
 
 target_include_directories(v2_infra PUBLIC
