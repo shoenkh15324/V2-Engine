cf6b6cb39aa5af4ed74c54e57fff487004b3b6a5
minkyukim <mok07333@gmail.com>
2026-07-07 09:32:32 +0900

    feat: add test infrastructure with BUILD_TESTING option
    - Add BUILD_TESTING option (default ON) to control test builds
    - Add test/ directory with Google Test via FetchContent
    - Add core_test.cmake with add_v2_test helper function
    - Add test_ring_buffer target skeleton

---
+++ test/unit/core/core_test.cmake
@@ -0,0 +1,9 @@
+function(add_v2_test name source)
+    add_executable(${name} ${CMAKE_CURRENT_LIST_DIR}/${source})
+
+    target_link_libraries(${name} PRIVATE v2_core GTest::gtest_main GTest::gmock)
+    target_include_directories(${name} PRIVATE ${CMAKE_SOURCE_DIR}/src)
+    gtest_discover_tests(${name})
+endfunction()
+
+add_v2_test(test_ring_buffer test_ring_buffer.cpp)
