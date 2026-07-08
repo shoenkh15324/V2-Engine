bc3e2bce483e292aad94e588a340402d4027b275
minkyukim <mok07333@gmail.com>
2026-07-08 16:38:25 +0900

    test(core): add unit tests for worker class

---
+++ test/unit/core/test_worker.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "core/actor_system/runtime/worker.hpp"
+#include "core/actor_system/runtime/dispatcher.hpp"
+
+TEST(Worker, Create){
+    Dispatcher d(1);
+    Worker w(&d, 0, 32);
+}
+
+TEST(Worker, StartStop){
+    Dispatcher d(1);
+    Worker w(&d, 0, 32);
+    w.start();
+    w.stop(); // thread join까지 완료
+}
+
+TEST(Worker, StartStopRepeated){
+    Dispatcher d(1);
+    Worker w(&d, 0, 32);
+    w.start();
+    w.stop();
+    w.start();
+    w.stop();
+}
